3f98a2a0af4997d8a2ee0a97aaa7ae42c0af7547
Sabrina <sabrina_teoh@hotmail.com>
2025-10-23 21:45:38 +1300

    refactor for 1.2 exercise

--- level1/courseInfo/src/Components/Content.jsx
+++ level1/courseInfo/src/Components/Content.jsx
@@ -1,9 +1,21 @@
-const Content = (props) => {
+import Part from "./Part";
+import Total from "./Total";
+
+const Content = () => {
+  const part1 = "Fundamentals of React";
+  const exercises1 = 10;
+  const part2 = "Using props to pass data";
+  const exercises2 = 7;
+  const part3 = "State of a component";
+  const exercises3 = 14;
+
+  const total = [exercises1, exercises2, exercises3];
   return (
     <>
-      <p>
-        {props.part} {props.exercise}
-      </p>
+      <Part part={part1} exercise={exercises1} />
+      <Part part={part2} exercise={exercises2} />
+      <Part part={part3} exercise={exercises3} />
+      <Total valuesArray={total} />
     </>
   );
 };
